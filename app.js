ddf4d065bd41fdd19dc37dfe2bf5311d3410c3bb
Usama129 <usama.humayun@ug.bilkent.edu.tr>
2020-12-12 14:56:24 +0300

    Replaced fetch-all endpoint with one for fetching by page number

--- app.js
+++ app.js
@@ -20,43 +20,49 @@ try {
     console.log("Cannot connect to Google Cloud - " + e )
 }
 
-app.get('/all', (req, res) => {
+app.get('/employees', (req, res) => {
     var ip = req.headers['x-real-ip'] || req.connection.remoteAddress
-    console.log("FETCH ALL request from " + ip + " at " + getTime())
-        getAllEmployees().then(result => {
-            res.json(result)
-        }).catch(err => {
-            console.log(err.code)
-            if (err.code === 'ETIMEDOUT'){
-                console.log("Connection to MySQL instance on Google Cloud timed out - check permissions and allowed network addresses on Google Cloud.")
-            } else if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
-                console.log("DB access was attempted after a fatal error. Redeploy!")
-            }
-            res.sendStatus(500)
-        })
+    console.log("FETCH EMPLOYEES request from " + ip + " at " + getTime())
+    getEmployees(req.query.page, req.query.items).then(result => {
+        res.json(result)
+    }).catch(err => {
+        console.log(err.code)
+        if (err.code === 'ETIMEDOUT'){
+             console.log("Connection to MySQL instance on Google Cloud timed out - check permissions and allowed network addresses on Google Cloud.")
+        } else if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+             console.log("DB access was attempted after a fatal error. Redeploy!")
+        }
+        res.sendStatus(500)
+     })
 })
 
-function getAllEmployees(){
+function getEmployees(page, rowsPerPage){
+
+    let offset = (rowsPerPage * page) - rowsPerPage
+
     return new Promise((resolve, reject) => {
-        con.query("select * from employee_data;", function (err, result, fields) {
-            if (err){ 
-                reject(err)
-              } else {
-                  employees = []
-                  for (row of result){
-                    row.birth_date = dateParser.parse("d/m/Y", row.birth_date)
-                    row.join_date = dateParser.parse("d/m/Y", row.join_date)
-                    if (row.gender === 'M'){
-                        row.gender = "Male"
-                    } else {
-                        row.gender = "Female"
+        con.query("select * from employee_data limit " + offset + ", " + rowsPerPage, 
+            function (err, result, fields) {
+                if (err){ 
+                    reject(err)
+                } else {
+                    employees = []
+                    var count = 0
+                    for (row of result){
+                        count += 1
+                        row.birth_date = dateParser.parse("d/m/Y", row.birth_date)
+                        row.join_date = dateParser.parse("d/m/Y", row.join_date)
+                        if (row.gender === 'M'){
+                            row.gender = "Male"
+                        } else {
+                            row.gender = "Female"
+                        }
+                        employees.push(row)
                     }
-                    employees.push(row)
-                  }
-                  resolve(employees)
-              }
-            //google_con.query("INSERT INTO employee_data (`employee_id`,`name`,`surname`,`join_date`,`gender`,`birth_date` ) VALUES('"+row.emp_no+"', '"+row.first_name+"','"+row.last_name+"','"+dateParser.parse('Y-m-d', row.hire_date)+"','"+row.gender+"','"+dateParser.parse('Y-m-d', row.birth_date)+"');")
-          })
+                    resolve({count: count, data: employees})
+                }
+                //google_con.query("INSERT INTO employee_data (`employee_id`,`name`,`surname`,`join_date`,`gender`,`birth_date` ) VALUES('"+row.emp_no+"', '"+row.first_name+"','"+row.last_name+"','"+dateParser.parse('Y-m-d', row.hire_date)+"','"+row.gender+"','"+dateParser.parse('Y-m-d', row.birth_date)+"');")
+            })
     })
     
 }
