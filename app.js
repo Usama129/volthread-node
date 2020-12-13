c36dffc56c84c1a6a6d68ce92136ebe6d6e70b10
Usama129 <usama.humayun@ug.bilkent.edu.tr>
2020-12-13 20:22:08 +0300

    Added endpoint, improved logging

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const mysql = require('mysql');
-const moment = require('moment-timezone');
+const moment = require('moment-timezone')
+const { body, validationResult } = require('express-validator');
 const dateParser = require('node-date-parser')
-var cors = require('cors');
+var cors = require('cors')
 const { ETIMEDOUT } = require("constants");
 const app = express()
 app.use(cors())
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+    require('dotenv').config()
   }
   
 
@@ -62,6 +63,52 @@ app.get('/count', (req, res) => {
     })
 })
 
+app.post('/add', express.json(), [
+    body("id").isInt(),
+    body("birth_date").isDate('DD/MM/YYYY'),
+    body("join_date").isDate('DD/MM/YYYY')
+],(req, res) => {
+    console.log("ADD EMPLOYEE request at " + getTime())
+
+    const valResult = validationResult(req);
+    if (!valResult.isEmpty()) {
+        for (item of valResult.errors){
+            console.log("Invalid value for " + item.param + ": " + item.value)
+        }
+        return res.status(400).json({ errors: valResult.array() });
+    }
+
+    addEmployee(req.body).then(result => {
+
+        console.log("Employee " + result.name + ", ID: " + result.id + " added successfully")
+        res.json({submit:"successful"})
+
+    }).catch(err => {
+
+
+        if (err === "bad data"){
+            console.log("Bad data sent by client")
+            res.status(400).json({submit:"failed", error:"Check data sent"})
+            return
+        }
+
+        if (err.sql){
+            console.log(err.sql.code)
+
+            if (err.sql.code === 'ER_DUP_ENTRY'){
+                console.log(err.id + " is duplicate.")
+                res.status(400).json({submit:"failed", error:"duplicate"})
+                return
+             }
+        } else {
+            console.log(err)
+        }
+
+        res.sendStatus(500)
+
+    })
+})
+
 function getEmployees(page, rowsPerPage){
 
     let offset = (rowsPerPage * page) - rowsPerPage
@@ -87,19 +134,54 @@ function getEmployees(page, rowsPerPage){
                     }
                     resolve({count: count, data: employees})
                 }
-                //google_con.query("INSERT INTO employee_data (`employee_id`,`name`,`surname`,`join_date`,`gender`,`birth_date` ) VALUES('"+row.emp_no+"', '"+row.first_name+"','"+row.last_name+"','"+dateParser.parse('Y-m-d', row.hire_date)+"','"+row.gender+"','"+dateParser.parse('Y-m-d', row.birth_date)+"');")
+                //google_con.query("")
             })
     })
     
 }
 
-function getEmployeeCount(){
+function getEmployeeCount(row){
+    
     return new Promise((resolve, reject) => {
-        con.query('select count(employee_id) as employee_count from employee_data', function(err, result, fields){
+        con.query("select count(employee_id) as employee_count from employee_data", function(err, result, fields){
             if (err){ 
                 reject(err)
             } else {
                 resolve(result)
+            }
+        })
+    })
+}
+
+function addEmployee(employee){
+    return new Promise((resolve, reject) => {
+        
+        if (!employee.id){
+            reject("bad data")
+        }
+    
+        row = employee
+        row.name = row.name.replace(/\s/g,'')
+        row.surname = row.surname.replace(/\s/g,'')
+    
+        row.join_date = moment(row.join_date, 'DD/MM/YYYY').toDate()
+        row.birth_date = moment(row.birth_date, 'DD/MM/YYYY').toDate()
+
+        if (row.gender === "Male" || row.gender === "male" || row.gender === "M" || row.gender === "m"){
+            row.gender = "M"
+        } else if (row.gender === "Female" || row.gender === "female" || row.gender === "F" || row.gender === "f"){
+            row.gender = "F"
+        } else {
+            reject("gender")
+            return
+        }
+
+        con.query("INSERT INTO employee_data (`employee_id`,`name`,`surname`,`join_date`,`gender`,`birth_date` ) VALUES('"+row.id+"', '"+row.name+"','"+row.surname+"','"+dateParser.parse('Y-m-d', row.join_date)+"','"+row.gender+"','"+dateParser.parse('Y-m-d', row.birth_date)+"');", 
+        function(err, result, fields){
+            if (err){ 
+                reject({id: row.id, sql: err})
+            } else {
+                resolve({name: row.name + " " + row.surname, id: row.id, sql: result})
             }
         })
     })
